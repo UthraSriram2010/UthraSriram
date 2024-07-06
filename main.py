8d9c5c3f4583f0353343f1f875dea2d09f114b77
UthraSriram2010 <uthra.srirams@gmail.com>
2024-07-06 20:15:34 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+def calculator():
+    a=int(input("Enter a number:"))
+    b=int(input("Enter a number:"))
+    c=input("Enter an operator to be used:")
+    if c=='+':
+        print("The sum of the two numbers is:", a+b)
+    if c=='-':
+        print("The difference of the two numbers is:", a-b)
+    if c=='*':
+        print("The product of the two numbers is:", a*b)
+    if c=='/':
+        print("The quotient of the two numbers is:", a-b)
+calculator()
+        
+        
+    
+     
+    
+
